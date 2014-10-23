f2afc05675b9db66100570ae3d30ab7a4dde2608
Poofplonker <michaelglenny@gmail.com>
2014-10-24 01:32:25 +1300

    Again tinkering with the classes

--- input/Pstransformer.cpp
+++ input/Pstransformer.cpp
@@ -13,13 +13,23 @@ int main(void){
     double a,b;
     int c;
     char waste;
+    double first[25];
+    double second[25];
+    int counter = 0;
     while (cin >> a >> waste >> b  >> waste >> c) {
+        first[c] += a;
+        second[c] += b;
+        counter++;
         cout << fixed <<setprecision(1) << a << "," << b <<",";
-        if(c <=9 || c > 21){
+        if(c < 7){
             cout << '0' << endl;
         }else{
             cout << '1' << endl;
         }
+        
     }
+//    for(int i = 0; i < 24; i++){
+//        cout  << i << " "<<first[i] /counter << " "<<  second[i]/counter << endl;
+//    }
 
 }
